470fcbac29ede645ff69a1f14d3c6a1a445746ad
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-17 17:43:35 +0300

    text

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "This program will calculate how many customers the cashier will have time to serve per shift." << std::endl;
+    std::cout << "---------------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "Enter shift duration in minutes: 480" << std::endl;
+    std::cout << "How many minutes does it take for a customer to place an order? 2" << std::endl;
+    std::cout << "How many minutes does it take for the cashier to collect the order? 4" << std::endl;
+    std::cout << "---------------We count---------------" << std::endl;
+    std::cout << "During a 480-minute shift, the cashier will have time to serve 80 customers." << std::endl;
+
+
 }
